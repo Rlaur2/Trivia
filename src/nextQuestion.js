1bf0d940d1a692f65d13d8307f3e66988a2933d2
Rlaur2 <plaureanopena@gmail.com>
2022-08-01 11:19:39 -0400

    Complete basic quiz functionality

--- src/nextQuestion.js
+++ src/nextQuestion.js
@@ -1,75 +1,48 @@
 import { shuffleArray } from "./quizConstruction";
-import { nextQuestion } from "./nextQuestion";
+import { endOfQuiz } from "./results";
 
-const quizDisplay = (questions) => {
-let currentQuestion = 1;
-const container = document.querySelector('.container');
-container.replaceChildren();
-container.innerHTML = 
-`<div class="row justify-content-end">
-<div class="col-2 progress">
-     
-</div>
-</div>
-<div class="row">
-<div class="col opening my-5">
-    <div class="row">
-        <div class="col result">
-            
-        </div>
-    </div>
-    <div class="question">
-        
-    </div>
-    <div class="row top answers">
-
-    </div>
-    <div class="row bottom answers">
-    
-    </div>
-    <div class="row submit-row">
-        <button class="btn col col-sm-1 btn-primary m-4 submit">Submit!</button>
-    </div>
-    <div class="row buttons">
-        <button class="btn col col-sm-1 btn-primary start-over m-4">Start Over!</button>
-    </div>
-</div>
-</div>`
-
-const progress = document.querySelector('.progress');
-progress.textContent = `Question ${currentQuestion}/${questions.length}`;
-const question = document.querySelector('.question');
-question.innerHTML = questions[currentQuestion - 1].question;
-const topRow = document.querySelector('.top');
-const bottomRow = document.querySelector('.bottom');
-
-if (questions[currentQuestion - 1].type === 'multiple') {
-    const allAnswers = questions[currentQuestion - 1].incorrect_answers;
-    allAnswers.push(questions[currentQuestion - 1].correct_answer);
-    shuffleArray(allAnswers);
-    allAnswers.forEach((item, index) => {
-        if (index < 2) {
-            const answer = document.createElement('div');
-            answer.classList.toggle('col');
-            answer.innerHTML = item;
-            topRow.appendChild(answer);
-        } else {
-            const answer = document.createElement('div');
-            answer.classList.toggle('col');
-            answer.innerHTML = item;
-            bottomRow.appendChild(answer);
-        }
-    });
-} else {
-    const trueAnswer = document.createElement('div');
-    trueAnswer.classList.toggle('col');
-    trueAnswer.textContent = 'True';
-    const falseAnswer = document.createElement('div');
-    falseAnswer.classList.toggle('col');
-    falseAnswer.textContent = 'False';
-    topRow.appendChild(trueAnswer);
-    bottomRow.appendChild(falseAnswer);
-};
+const nextQuestion = (questions, currentQuestion, correctAnswers) => {
+    currentQuestion++;
+    //dom elements that will change each question
+    const progress = document.querySelector('.progress');
+    progress.textContent = `Question ${currentQuestion}/${questions.length}`
+    const question = document.querySelector('.question');
+    question.innerHTML = questions[currentQuestion - 1].question;
+    const result = document.querySelector('.result');
+    result.classList.remove('right');
+    result.classList.remove('wrong');
+    result.textContent = '';
+    const topRow = document.querySelector('.top');
+    topRow.replaceChildren();
+    const bottomRow = document.querySelector('.bottom');
+    bottomRow.replaceChildren();
+    if (questions[currentQuestion - 1].type === 'multiple') {
+        const allAnswers = questions[currentQuestion - 1].incorrect_answers;
+        allAnswers.push(questions[currentQuestion - 1].correct_answer);
+        shuffleArray(allAnswers);
+        allAnswers.forEach((item, index) => {
+            if (index < 2) {
+                const answer = document.createElement('div');
+                answer.classList.toggle('col');
+                answer.innerHTML = item;
+                topRow.appendChild(answer);
+            } else {
+                const answer = document.createElement('div');
+                answer.classList.toggle('col');
+                answer.innerHTML = item;
+                bottomRow.appendChild(answer);
+            }
+        });
+    } else {
+        const trueAnswer = document.createElement('div');
+        trueAnswer.classList.toggle('col');
+        trueAnswer.textContent = 'True';
+        const falseAnswer = document.createElement('div');
+        falseAnswer.classList.toggle('col');
+        falseAnswer.textContent = 'False';
+        topRow.appendChild(trueAnswer);
+        bottomRow.appendChild(falseAnswer);
+    };
 const answerSelections = document.querySelectorAll('.answers>.col');
 let answerChosen = '';
 const selectAnswer = (e) => {
@@ -81,10 +54,11 @@ const selectAnswer = (e) => {
 };
 topRow.addEventListener('mousedown', selectAnswer);
 bottomRow.addEventListener('mousedown', selectAnswer);
-let correctAnswers = 0;
+const buttonsRow = document.querySelector('.buttons');
+const oldNext = document.querySelector('.next');
+buttonsRow.removeChild(oldNext);
 const submitButton = document.querySelector('.submit');
 const submitAction = () => {
-    const result = document.querySelector('.result');
     if (!answerChosen) {
         alert('Please choose an answer.');
         return;
@@ -106,7 +80,7 @@ const submitAction = () => {
     submitButton.removeEventListener('mousedown', submitAction);
     topRow.removeEventListener('mousedown', selectAnswer);
     bottomRow.removeEventListener('mousedown', selectAnswer);
-    const buttonsRow = document.querySelector('.buttons');
+    
     if (currentQuestion === questions.length) {
         const finishButton = document.createElement('button');
         finishButton.classList.toggle('btn');
@@ -136,6 +110,6 @@ const submitAction = () => {
     };
 }; 
 submitButton.addEventListener('mousedown', submitAction);
-};
+}
 
-export {quizDisplay};
+export {nextQuestion};
---
+++ src/results.js
@@ -0,0 +1,116 @@
+const endOfQuiz = (questions, correctAnswers) => {
+    const container = document.querySelector('.container');
+    container.replaceChildren();
+    container.innerHTML = 
+    `<div class="row justify-content-end">
+    <div class="col-2 progress">
+        End of quiz
+    </div>
+</div>
+<div class="row">
+    <div class="col opening my-5">
+        <div class="row justify-content-center">
+            <div class="col-6 adulation">
+                
+            </div>
+        </div>
+        <div class="row">
+            <div class="col evaluation">
+            
+            </div>
+        </div>
+        <div class="row">
+            <div class="col giphy">
+                <img src="" alt="">
+            </div>
+        </div>
+        <div class="row buttons">
+            <button class="btn col col-sm-1 btn-primary back m-4">Review!</button>
+            <button class="btn col col-sm-1 btn-primary next m-4">New Quiz!</button>
+        </div>
+        <div class="row">
+            <div class="col attribution">
+                <a href="https://opentdb.com">Powered by the Open Trivia Database</a>
+            </div>
+        </div>
+    </div>
+</div>`
+
+let resultsString = (correctAnswers / questions.length).toFixed(2);
+let resultsStraight = '';
+if (resultsString === '1.00') {
+    resultsStraight = '100';
+} else if (resultsString === '0.00') {
+    resultsStraight = '0';
+} else {
+    resultsString = resultsString.slice(2,4);
+    if (resultsString[0] === '0') {
+        resultsString = resultsString.slice(1);
+    }
+    resultsStraight = resultsString;
+}
+const adulation = document.querySelector('.adulation');
+let rng = 0;
+if (resultsStraight === '100') {
+    adulation.classList.add('excellent');
+    adulation.textContent = 'Perfect!'
+} else if ( resultsStraight < 100 && resultsStraight > 79) {
+    adulation.classList.add('excellent');
+    rng = Math.floor(Math.random()*3);
+    if (rng === 0) {
+        adulation.textContent = 'Congratulations!'
+    } else if (rng === 1) {
+        adulation.textContent = 'Amazing!';
+    } else {
+        adulation.textContent = 'Great!';
+    }
+} else if (resultsStraight <= 79 && resultsStraight > 59) {
+    adulation.classList.add('good');
+    rng = Math.floor(Math.random()*3);
+    if (rng === 0) {
+        adulation.textContent = 'Good job!';
+    } else if (rng === 1) {
+        adulation.textContent = 'Good!'
+    } else {
+        adulation.textContent = 'Alright!'
+    }
+} else if (resultsStraight <= 59 && resultsStraight > 44) {
+    adulation.classList.add('mediocre');
+    rng = Math.floor(Math.random()*3);
+    if (rng === 0) {
+        adulation.textContent = 'Nice try!';
+    } else if (rng === 1) {
+        adulation.textContent = 'Better luck next time!';
+    } else {
+        adulation.textContent = 'Good effort!'
+    }
+} else {
+    adulation.classList.add('awful');
+    rng = Math.floor(Math.random()*4);
+    if (rng === 0) {
+        adulation.textContent = 'Failure';
+    } else if (rng === 1 ) {
+        adulation.textContent = 'Awful';
+    } else if (rng === 2) {
+        adulation.textContent = 'Disgraceful'
+    } else {
+        adulation.textContent = 'Dumbass';
+    }
+};
+const evaluation = document.querySelector('.evaluation');
+evaluation.textContent = `You scored: ${correctAnswers} out of ${questions.length} - ${resultsStraight}% correct`;
+const img = document.querySelector('img');
+const displayGif = async () => {
+    try {
+        const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=h2c3vzphvIByUtBEb1rnOHMANHfHBmnj&s=${adulation.textContent}`, {mode: 'cors'});
+        const output = await response.json();
+        img.src = output.data.images.fixed_height.url; 
+    } catch(error) {
+        alert(error);
+    }
+};
+displayGif();
+
+
+}
+export {endOfQuiz};
